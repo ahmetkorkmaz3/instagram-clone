cd967ea3e870e22579792da92e527825852bd5cd
Ahmet <ahmetkorkmaz3453@gmail.com>
2021-02-01 20:19:50 +0300

    Add Component: CustomText

---
+++ src/components/CustomText.vue
@@ -0,0 +1,38 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    size: {
+      type: String,
+      default: 'normal',
+      validator: function(value) {
+        return ['normal', 'small', 'x-small', 'xx-small'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.small {
+  font-size: 12px;
+}
+
+.x-small {
+  font-size: 11px;
+}
+
+.xx-small {
+  font-size: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText tag="p">Hello</CustomText>
+    <CustomText tag="b" size="x-small">Hello</CustomText>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import CustomText from '@/components/CustomText'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    CustomText
+  }
 }
 </script>
